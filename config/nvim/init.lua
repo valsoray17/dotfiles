e5e72059f7833c8bf19c70b38f3f9539417a89ed
Yaroslav Sapozhnyk <yaroslav.sapozhnik@gmail.com>
2025-10-13 23:31:53 -0400

    Add neovim configs and Lazy pkg manager

---
+++ config/nvim/init.lua
@@ -0,0 +1,11 @@
+require("config.lazy")
+require("catppuccin").setup()
+require('lualine').setup()
+
+--set the colorscheme to it!
+vim.cmd.colorscheme "catppuccin"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
