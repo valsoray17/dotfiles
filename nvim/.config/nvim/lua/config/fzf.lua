4cb382bac81517eb5bdd9369724902d552c01b15
Yaroslav Sapozhnyk <ysapozhnyk@fubo.tv>
2026-03-18 17:04:06 -0400

    Only load recent files from the current dir

--- nvim/.config/nvim/lua/config/fzf.lua
+++ nvim/.config/nvim/lua/config/fzf.lua
@@ -3,7 +3,9 @@ local fzf = require("fzf-lua")
 -- files / buffers / recent
 vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Buffers" })
-vim.keymap.set("n", "<leader>fr", fzf.oldfiles, { desc = "Recent files" })
+vim.keymap.set("n", "<leader>fr", function()
+  fzf.oldfiles({ cwd_only = true })
+end, { desc = "Recent files (cwd)" })
 vim.keymap.set("n", "<leader>fg", fzf.git_files, { desc = "Git files" })
 vim.keymap.set("n", "<leader>/", fzf.live_grep, { desc = "Live grep" })
 
